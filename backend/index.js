001b1b69e7098f37d2751610b9d5365e132fe2a0
OgaitnaSZ <zanty201212@gmail.com>
2025-07-13 22:21:06 -0300

    Se agrego la configuracion de la db y se creo el index.js

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const conectarDB = require('./config/db')
+const cors = require('cors');
+
+// Crear servidor
+const app = express();
+
+// Conectar db
+conectarDB();
+app.use(cors());
+
+app.use(express.json());
+
+//app.use('/api/productos', require('./routes/producto'));
+
+app.listen(4000, ()=>{
+    console.log('El servidor esta corriendo');
+})
